0965edafc5e41ae6d1ba1797ac6c0f0a67e0a1ad
Jack <jack.branthwaite@gmail.com>
2021-03-03 11:17:06 +1300

    Database connected with environment variable

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 //Connect to DB
-mongoose.connect( , {useNewUrlParser: true}, () => console.log("Database Connected"));
+mongoose.connect(process.env.DB, { useNewUrlParser: true }, () =>
+  console.log("Database Connected")
+);
 
 //Import Routes
 const authRoute = require("./Routes/auth");
