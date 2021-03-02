5d25bba930463517f83e9554028765ccd3fe123c
Jack <jack.branthwaite@gmail.com>
2021-03-03 10:56:39 +1300

    changes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+//Connect to DB
+mongoose.connect( , {useNewUrlParser: true}, () => console.log("Database Connected"));
 
 //Import Routes
 const authRoute = require("./Routes/auth");
@@ -7,4 +11,4 @@ const authRoute = require("./Routes/auth");
 //Route Middleware
 app.use("/api/user", authRoute);
 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3000, () => console.log("Server Running"));
