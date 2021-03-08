de529395906f4dc59522b8d1f2302f7e58361e2f
Jack <jack.branthwaite@gmail.com>
2021-03-08 17:31:26 +1300

    validation using hapi joi added

---
+++ validation.js
@@ -0,0 +1,26 @@
+//Validation
+const joi = require("@hapi/joi");
+
+//Register validation
+const registerValidation = (data) => {
+  const schema = {
+    name: joi.string().min(6).required(),
+    email: joi.string().min(6).required().email(),
+    password: joi.string().min(6).required(),
+  };
+
+  return schema.validate(data);
+};
+
+//Login validation
+const loginValidation = (data) => {
+  const schema = {
+    email: joi.string().min(6).required().email(),
+    password: joi.string().min(6).required(),
+  };
+
+  return joi.validate(data, schema);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
