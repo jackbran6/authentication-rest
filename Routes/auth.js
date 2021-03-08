65b21570a61ddeb71bd65a1336bdd4e3e632b562
Jack <jack.branthwaite@gmail.com>
2021-03-08 17:49:52 +1300

    hashing and salting through bcrypt

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 const User = require("../model/User");
 const { registerValidation } = require("../validation");
 
@@ -11,11 +12,15 @@ router.post("/register", async (req, res) => {
   const emailExists = await User.findOne({ email: req.body.email });
   if (emailExists) return res.status(400).send("Email already exists");
 
+  //Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   //New user created
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
